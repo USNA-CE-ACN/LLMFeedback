a446364c6dcdfe165f370dc6f5bc0d8349ba0e40
Christian DeLozier <crdelozier@gmail.com>
2025-05-12 15:02:31 -0400

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'smkkyxmy_llm');
-   define('DB_PASSWORD', 'llmFEEDB@CK2024');
-   define('DB_DATABASE', 'smkkyxmy_llmfeedback');
-   $db = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
