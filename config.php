4c5484648634ea871da07e6167596fe812075aaf
Christian DeLozier <delozier@usna.edu>
2025-05-19 15:43:22 -0400

    Adding configuration file blank (public facing)

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', '');
+   define('DB_USERNAME', '');
+   define('DB_PASSWORD', '');
+   define('DB_DATABASE', '');
+   $db = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
