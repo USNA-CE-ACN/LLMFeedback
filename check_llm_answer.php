4c6a6bdcb4eaa51a047fe64d26e2ad27e4916946
Christian DeLozier <delozier@usna.edu>
2025-07-02 08:48:48 -0400

    Updated to local llama model

--- check_llm_answer.php
+++ check_llm_answer.php
@@ -88,6 +88,16 @@ if ($db->connect_error) {
 		$sql->close();
 
 		require_once 'vendor/autoload.php';
+		$client = \ArdaGnsrn\Ollama\Ollama::client();
+		
+		$completions = $client->completions()->create([
+			'model' => 'gemma3',
+			'prompt' => $prompt,
+		]);
+
+		$response = $completions->response;
+
+	/*
 		$yourApiKey = file_get_contents("lab_api_key.key");
 		$client = OpenAI::client($yourApiKey);
 
@@ -96,6 +106,7 @@ if ($db->connect_error) {
 			'messages' => [['role' => 'user', 'content' => $prompt],],]);
 
  		$response = $result->choices[0]->message->content;
+		*/
 		preg_match('/\d+/', $response, $matches);
 		$rating = $matches[0];
 		$feedback = strstr($response,"Feedback");
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -10,7 +10,6 @@ class ComposerStaticInit150000716fb3a1739eaf9bf6a941d41f
         '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
         '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
         '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
-        '61ba3e92ef91a7a0f1cc260ebd9290e4' => __DIR__ . '/..' . '/openai-php/client/src/OpenAI.php',
     );
 
     public static $prefixLengthsPsr4 = array (
@@ -19,20 +18,15 @@ class ComposerStaticInit150000716fb3a1739eaf9bf6a941d41f
             'Psr\\Http\\Message\\' => 17,
             'Psr\\Http\\Client\\' => 16,
         ),
-        'O' => 
-        array (
-            'OpenAI\\' => 7,
-        ),
-        'H' => 
-        array (
-            'Http\\Message\\MultipartStream\\' => 29,
-            'Http\\Discovery\\' => 15,
-        ),
         'G' => 
         array (
             'GuzzleHttp\\Psr7\\' => 16,
             'GuzzleHttp\\Promise\\' => 19,
             'GuzzleHttp\\' => 11,
+        ),
+        'A' => 
+        array (
+            'ArdaGnsrn\\Ollama\\' => 17,
         ),
     );
 
@@ -46,18 +40,6 @@ class ComposerStaticInit150000716fb3a1739eaf9bf6a941d41f
         array (
             0 => __DIR__ . '/..' . '/psr/http-client/src',
         ),
-        'OpenAI\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/openai-php/client/src',
-        ),
-        'Http\\Message\\MultipartStream\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/php-http/multipart-stream-builder/src',
-        ),
-        'Http\\Discovery\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/php-http/discovery/src',
-        ),
         'GuzzleHttp\\Psr7\\' => 
         array (
             0 => __DIR__ . '/..' . '/guzzlehttp/psr7/src',
@@ -69,6 +51,10 @@ class ComposerStaticInit150000716fb3a1739eaf9bf6a941d41f
         'GuzzleHttp\\' => 
         array (
             0 => __DIR__ . '/..' . '/guzzlehttp/guzzle/src',
+        ),
+        'ArdaGnsrn\\Ollama\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/ardagnsrn/ollama-php/src',
         ),
     );
 
